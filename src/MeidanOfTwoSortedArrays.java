faee0dff28259588f52af4ea4717bf74073e3dcd
Phuoc Ho <vanphuoc3012@gmail.com>
2023-01-07 03:08:25 +0700

    Median of Two Sorted Arrays

---
+++ src/MeidanOfTwoSortedArrays.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class MeidanOfTwoSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length + nums2.length];
+        int i = 0;
+        int j = 0;
+        for(int k = 0; k < arr.length; k++) {
+            if (i >= nums1.length) arr[k] = nums2[j++];
+            else if (j >= nums2.length) arr[k] = nums1[i++];
+            else if (nums1[i] < nums2[j]) arr[k] = nums1[i++];
+            else arr[k] = nums2[j++];
+        }
+        System.out.println(Arrays.toString(arr));
+        if(arr.length % 2 == 0) {
+            int midIndex = arr.length / 2;
+            return (double) (arr[midIndex - 1] + arr[midIndex]) / 2;
+        } else {
+            return arr[arr.length / 2];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+
+        double x = findMedianSortedArrays(nums1, nums2);
+        System.out.println(x);
+    }
+}
